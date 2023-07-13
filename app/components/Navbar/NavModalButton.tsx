7a1a1d0b4af6931ac9791dbcf7ebcaf37e0e8dce
CandraSetiawan <candra12305@gmail.com>
2023-07-13 23:38:04 +0700

    add NavModalButton

---
+++ app/components/Navbar/NavModalButton.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import hamberger from "../../assets/img/hamberger.svg";
+
+type ToggleModalProps = {
+  toggleModal: () => void;
+};
+
+const NavModalButton = ({ toggleModal }: ToggleModalProps) => {
+  return (
+    <div className="md:hidden flex items-center">
+      {/* button modal */}
+      <button
+        className=" bg-slate-500 dark:bg-slate-700 rounded-md"
+        onClick={toggleModal}
+      >
+        <Image src={hamberger} alt="menu icon" width={40} height={40} />
+      </button>
+    </div>
+  );
+};
+
+export default NavModalButton;
