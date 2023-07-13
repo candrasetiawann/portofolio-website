095e5dafb782dc1aa79e2bf4a9c9ddc6a20bf8b7
CandraSetiawan <candra12305@gmail.com>
2023-07-13 23:32:04 +0700

    add DownloadResumeButton

---
+++ app/components/Home/DownloadResumeButton.tsx
@@ -0,0 +1,12 @@
+const DownloadResumeButton = () => {
+  return (
+    <a
+      className="border text-slate-700 border-slate-700 dark:text-sky-600 dark:border-sky-600 py-2 px-4 "
+      href=""
+    >
+      Download Resume
+    </a>
+  );
+};
+
+export default DownloadResumeButton;
