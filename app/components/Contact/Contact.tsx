adcecaa7e567d9c341ceb52ad8becdc19af61ce8
CandraSetiawan <candra12305@gmail.com>
2023-07-13 23:30:55 +0700

    add Contact

---
+++ app/components/Contact/Contact.tsx
@@ -0,0 +1,44 @@
+import Link from "next/link";
+
+const contactItem = [
+  {
+    name: "Github",
+    username: "candrasetiawann",
+    url: "https://github.com/candrasetiawann",
+  },
+  {
+    name: "Linkedin",
+    username: "Candra Setiawan",
+    url: "https://www.linkedin.com/in/candra-setiawan-3462a21b6/",
+  },
+  {
+    name: "Email",
+    username: "candrasetiawan0630@gmail.com",
+    url: "candrasetiawan0630@gmail.com",
+  },
+];
+
+const ContactCard = () => {
+  return (
+    <div className="flex flex-col pl-6 pt-4">
+      {contactItem.map((contact) => (
+        <ul key={contact.name} className="list-disc dark:list-white">
+          <li>
+            <Link
+              className="font-bold text-neutral-700 dark:text-sky-600"
+              target="_blank"
+              href={contact.url}
+            >
+              {contact.name} -{" "}
+              <span className="border-neutral-700 border-b border-dotted">
+                {contact.username}
+              </span>
+            </Link>
+          </li>
+        </ul>
+      ))}
+    </div>
+  );
+};
+
+export default ContactCard;
