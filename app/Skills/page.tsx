2671aa566f10bd9b61c491ada2f9c03a1b1e73ec
CandraSetiawan <candra12305@gmail.com>
2023-08-17 10:29:11 +0700

    feat: Import TechStack.tsx component

--- app/Skills/page.tsx
+++ app/Skills/page.tsx
@@ -1,12 +1,14 @@
-import TabSkills from "../components/Skills/Tabs";
+// import TabSkills from "../components/Skills/Tabs";
+import TechStack from "../components/Skills/TechStack";
 const Skills = () => {
   return (
-    <div className="md:h-full flex flex-col px-8 md:px-0">
-      <h1 className="text-neutral-700 text-3xl font-bold pt-16 dark:text-textgray">
+    <div className="md:h-full flex flex-col px-8 md:px-0 lg:pb-14 ">
+      <h1 className="text-neutral-700 text-3xl font-bold pt-16 pb-3 lg:pt-2 dark:text-textgray">
         Skills
       </h1>
-      <div className="flex md:flex-row pb-8 dark:text-textgray">
-        <TabSkills />
+      <div className="flex md:flex-row dark:text-textgray">
+        {/* <TabSkills /> */}
+        <TechStack />
       </div>
     </div>
   );
