f38ac029be5e829260877b4d41454399616a37be
CandraSetiawan <candra12305@gmail.com>
2023-08-24 10:36:06 +0700

    upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,14 +12,18 @@ module.exports = {
   theme: {
     colors: {
       "bgdark": "#1E293B",
+      "dark" : "#263856",
       "textgray": "#CBD5E1",
       "gray": "#CBD5E1",
       "amber-500" : "#f59e0b",
       "slate-500":"#64748b",
       "slate-100" : "#f1f5f9",
+      "slate-200" : "#e2e8f0",
       "neutral-700" : "#404040",
       "white" : "#fff",
-      "sky-600":"#0284c7"
+      "sky-600":"#0284c7",
+      "blue-500" : "#1E4FD9",
+      "red-500" : "#EF4444",
     },
     extend: {},
   },
